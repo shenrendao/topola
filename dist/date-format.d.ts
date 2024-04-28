ec0401ec600c9f93bc5cbc4fec94787f278a640f
shenrendao <shenrshen@webank.com>
2024-04-28 13:15:48 +0800

    Revert "add build output to git"
    This reverts commit c4e4b4706f46f56a4a0991c05a39bcd88149553a.

--- dist/date-format.d.ts
+++
@@ -1,5 +0,0 @@
-import { Date as GedcomDate, DateOrRange } from './data';
-/** Simple date formatter. */
-export declare function formatDate(date: GedcomDate, locale?: string): string;
-/** Formats a DateOrRange object. */
-export declare function formatDateOrRange(dateOrRange: DateOrRange, locale?: string): string;
--- dist/id-generator.d.ts
+++
@@ -1,9 +0,0 @@
-/** Provides unique identifiers. */
-export declare class IdGenerator {
-    readonly ids: Map<string, number>;
-    /**
-     * Returns the given identifier if it wasn't used before. Otherwise, appends
-     * a number to the given identifier to make it unique.
-     */
-    getId(id: string): string;
-}
